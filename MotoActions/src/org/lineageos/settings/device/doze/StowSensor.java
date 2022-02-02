9fe843d4ee7fa810aacfd356a6f4782cb94d5c72
dianlujitao <dianlujitao@lineageos.org>
2022-02-02 18:07:22 +0800

    MotoActions: Use stow sensor for both hand wave and pick up gestures
     * The behaviors of stow and proximity sensors are pretty similar, so
       there's no need to use both at the same time. Simply use the stow
       sensor to handle both hand wave and pick up gestures.
     * Switch to timestamp associated with the sensor event to determine the
       operation while at it.
    
    Change-Id: I39767cf7b2e48bc2b0baabfac1e47240012f71d6

--- MotoActions/src/org/lineageos/settings/device/doze/StowSensor.java
+++ MotoActions/src/org/lineageos/settings/device/doze/StowSensor.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2015 The CyanogenMod Project
- * Copyright (c) 2017 The LineageOS Project
+ * Copyright (c) 2017-2022 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,15 +22,18 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.util.Log;
 
-import java.lang.System;
-
 import org.lineageos.settings.device.MotoActionsSettings;
 import org.lineageos.settings.device.SensorAction;
 import org.lineageos.settings.device.SensorHelper;
 
 public class StowSensor implements ScreenStateNotifier, SensorEventListener {
     private static final String TAG = "MotoActions-StowSensor";
-    private static final int IN_POCKET_MIN_TIME = 5000;
+
+    // Maximum time for the hand to cover the sensor: 1s
+    private static final int HANDWAVE_MAX_DELTA_NS = 1000 * 1000 * 1000;
+
+    // Minimum time until the device is considered to have been in the pocket: 5s
+    private static final int POCKET_MIN_DELTA_NS = 5000 * 1000 * 1000;
 
     private final MotoActionsSettings mMotoActionsSettings;
     private final SensorHelper mSensorHelper;
@@ -39,7 +42,7 @@ public class StowSensor implements ScreenStateNotifier, SensorEventListener {
 
     private boolean mEnabled;
     private boolean mLastStowed;
-    private long isStowedTime;
+    private long mLastStowedTime;
 
     public StowSensor(MotoActionsSettings MotoActionsSettings, SensorHelper sensorHelper,
                 SensorAction action) {
@@ -61,7 +64,8 @@ public class StowSensor implements ScreenStateNotifier, SensorEventListener {
 
     @Override
     public void screenTurnedOff() {
-        if (mMotoActionsSettings.isPocketGestureEnabled() && !mEnabled) {
+        if ((mMotoActionsSettings.isPocketGestureEnabled() ||
+                    mMotoActionsSettings.isIrWakeupEnabled()) && !mEnabled) {
             Log.d(TAG, "Enabling");
             mSensorHelper.registerListener(mSensor, this);
             mEnabled = true;
@@ -71,17 +75,31 @@ public class StowSensor implements ScreenStateNotifier, SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
         boolean thisStowed = (event.values[0] != 0);
-        if(thisStowed){
-            isStowedTime = System.currentTimeMillis();
+        if (thisStowed) {
+            mLastStowedTime = event.timestamp;
         } else if (mLastStowed && !thisStowed) {
-            long inPocketTime = System.currentTimeMillis() - isStowedTime;
-            if(inPocketTime >= IN_POCKET_MIN_TIME){
-                Log.d(TAG, "Triggered after " + inPocketTime / 1000 + " seconds");
+            if (shouldPulse(event.timestamp)) {
                 mSensorAction.action();
             }
         }
         mLastStowed = thisStowed;
         Log.d(TAG, "event: " + thisStowed);
+    }
+
+    private boolean shouldPulse(long timestamp) {
+        long delta = timestamp - mLastStowedTime;
+
+        boolean irWakeupEnabled = mMotoActionsSettings.isIrWakeupEnabled();
+        boolean pocketGestureEnabled = mMotoActionsSettings.isPocketGestureEnabled();
+
+        if (irWakeupEnabled && pocketGestureEnabled) {
+            return true;
+        } else if (irWakeupEnabled) {
+            return delta < HANDWAVE_MAX_DELTA_NS;
+        } else if (pocketGestureEnabled) {
+            return delta >= POCKET_MIN_DELTA_NS;
+        }
+        return false;
     }
 
     @Override
