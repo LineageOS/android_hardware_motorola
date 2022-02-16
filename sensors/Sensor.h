b56cedde1f0c98bcf3064c14adb714af75025e21
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-17 01:06:04 +0200

    sensors: Make sensor flush function virtual
    Change-Id: I46532fc726d9e0c2073100ff1f6008fcde9e821c

--- sensors/Sensor.h
+++ sensors/Sensor.h
@@ -40,7 +40,7 @@ class Sensor {
     const SensorInfo& getSensorInfo() const;
     virtual void batch(int32_t samplingPeriodNs);
     virtual void activate(bool enable);
-    Result flush();
+    virtual Result flush();
 
     void setOperationMode(OperationMode mode);
     bool supportsDataInjection() const;
