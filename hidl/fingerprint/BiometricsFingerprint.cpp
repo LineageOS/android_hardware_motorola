bb693f6193a5d9804fae76619f55d23903546df0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-05 15:53:23 +0200

    hidl: biometrics: fingerprint: Drop LOG_VERBOSE
    * It's useless since it's the same as LOG_TAG
    
    Change-Id: I1a15ea85f840b7af6d41b59cf01b0ec3c2457705

--- hidl/fingerprint/BiometricsFingerprint.cpp
+++ hidl/fingerprint/BiometricsFingerprint.cpp
@@ -4,8 +4,8 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.moto"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.moto"
 
 #include <hardware/hw_auth_token.h>
 
