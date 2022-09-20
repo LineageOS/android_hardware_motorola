23de3c7d47c117523c956d7bc6f0302b358469e9
Nolen Johnson <johnsonnolen@gmail.com>
2022-09-20 18:18:04 -0400

    Provide a shim for libqsap
    * Android.mk because we link libqsap_sdk which is Android.mk.
    
    Co-authored-by: Dan Pasanen <dan.pasanen@gmail.com>
    Co-authored-by: Erfan Abdi <erfangplus@gmail.com>
    Co-authored-by: Prema Chand Alugu <premaca@gmail.com>
    Co-authored-by: Jeferson <jroliveira.oliveira301@gmail.com>
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: I97b856d1bf6d390c65cb0043e8ffe4d3d0d55d75

---
+++ libqsap_shim/Android.mk
@@ -0,0 +1,17 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := libqsap_shim.c
+LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog libcutils libutils
+LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
+LOCAL_MODULE := libqsapshim
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
