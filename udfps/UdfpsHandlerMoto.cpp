e7c8ef0abdca0a798410fa57f538d6b50ff5c91e
electimon <electimon@gmail.com>
2024-01-07 10:51:54 +0000

    udfps: Switch to IOCTL for HBM
    * On newer moto kernels it is possible
      to send a SET_PANEL_FEATURE ioctl to /dev/dri/card0
      to set a panel feature such as HBM. Motorola has
      extended the HBM logic to allow FOD HBM to be set
      in additional to regular HBM. This is done by sending
      "2" with the HBM id through the ioctl.
    
    Change-Id: I06ace29567900779e63b9826862964d20b267be6

--- udfps/UdfpsHandlerMoto.cpp
+++ udfps/UdfpsHandlerMoto.cpp
@@ -6,19 +6,12 @@
 
 #define LOG_TAG "UdfpsHandler.moto"
 
-#include <android-base/file.h>
-#include <android-base/logging.h>
 #include <com/motorola/hardware/biometric/fingerprint/1.0/IMotoFingerPrint.h>
 
-#include <fcntl.h>
-#include <poll.h>
 #include <chrono>
-#include <fstream>
 #include <thread>
 
-#include "UdfpsHandler.h"
-
-#define FOD_HBM_PATH "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/fod_hbm"
+#include "UdfpsHandlerMoto.h"
 
 using ::android::sp;
 using ::android::hardware::hidl_vec;
@@ -61,7 +54,7 @@ class MotoUdfpsHandler : public UdfpsHandler {
 
         mMotoFingerprint->sendFodEvent(IMotFodEventType::FINGER_UP, {},
                                        [](IMotFodEventResult, const hidl_vec<signed char>&) {});
-        android::base::WriteStringToFile("0", FOD_HBM_PATH);
+        setHbmState(OFF);
 
         mHbmFodEnabled = false;
     }
@@ -73,7 +66,7 @@ class MotoUdfpsHandler : public UdfpsHandler {
             return;
         }
 
-        android::base::WriteStringToFile("1", FOD_HBM_PATH);
+        setHbmState(ON);
         mMotoFingerprint->sendFodEvent(IMotFodEventType::FINGER_DOWN, {},
                                        [](IMotFodEventResult, const hidl_vec<signed char>&) {});
 
