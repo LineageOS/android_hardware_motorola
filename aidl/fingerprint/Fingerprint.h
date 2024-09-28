695dfc31f4de2da855c14abab07ce5de0fe99fcd
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-09-28 16:26:08 +0200

    aidl: fingerprint: Initial Motorola implementation
    Co-authored-by: IPSBHANGU <inderpreetbhangu2@gmail.com>
    Change-Id: Ib410ef5d92747deb9af34b3eb88f620c75797618

--- aidl/fingerprint/Fingerprint.h
+++ aidl/fingerprint/Fingerprint.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2024 The LineageOS Project
+ *               2024 Paranoid Android
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -8,6 +9,12 @@
 
 #include <aidl/android/hardware/biometrics/fingerprint/BnFingerprint.h>
 
+#include "FingerprintConfig.h"
+#include "LockoutTracker.h"
+#include "Session.h"
+#include "UdfpsHandler.h"
+
+using ::aidl::android::hardware::biometrics::fingerprint::FingerprintSensorType;
 using ::aidl::android::hardware::biometrics::fingerprint::ISession;
 using ::aidl::android::hardware::biometrics::fingerprint::ISessionCallback;
 using ::aidl::android::hardware::biometrics::fingerprint::SensorProps;
@@ -16,10 +23,27 @@ namespace aidl::android::hardware::biometrics::fingerprint {
 
 class Fingerprint : public BnFingerprint {
   public:
+    Fingerprint(std::shared_ptr<FingerprintConfig> config);
+    ~Fingerprint();
+
     ndk::ScopedAStatus getSensorProps(std::vector<SensorProps>* _aidl_return) override;
     ndk::ScopedAStatus createSession(int32_t sensorId, int32_t userId,
                                      const std::shared_ptr<ISessionCallback>& cb,
                                      std::shared_ptr<ISession>* out) override;
+
+  private:
+    fingerprint_device_t* openFingerprintHal(const char* class_name);
+    SensorLocation getSensorLocation();
+    static void notify(const fingerprint_msg_t* msg);
+
+    std::shared_ptr<FingerprintConfig> mConfig;
+    std::shared_ptr<Session> mSession;
+    LockoutTracker mLockoutTracker;
+    FingerprintSensorType mSensorType;
+
+    fingerprint_device_t* mDevice;
+    UdfpsHandlerFactory* mUdfpsHandlerFactory;
+    UdfpsHandler* mUdfpsHandler;
 };
 
 }  // namespace aidl::android::hardware::biometrics::fingerprint
