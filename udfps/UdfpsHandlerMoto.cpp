c9239964b883faee3014b0d589043f254df5a301
Arian <arian.kulmer@web.de>
2025-05-24 13:15:15 +0200

    udfps: Disable hbm on most acquired events
    Change-Id: I0d2e340a553a4c9e41666f6f422805cecb15a68d

--- udfps/UdfpsHandlerMoto.cpp
+++ udfps/UdfpsHandlerMoto.cpp
@@ -66,8 +66,11 @@ class MotoUdfpsHandler : public UdfpsHandler {
 
     void onFingerUp() { disableHighBrightFod(); }
 
-    void onAcquired(int32_t /*result*/, int32_t /*vendorCode*/) {
-        // nothing
+    void onAcquired(int32_t result, int32_t /*vendorCode*/) {
+        if (result == FINGERPRINT_ACQUIRED_GOOD) {
+            // Set finger as up to disable HBM already, even if the finger is still pressed
+            onFingerUp();
+        }
     }
 
     void cancel() {
