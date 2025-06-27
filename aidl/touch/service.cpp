dd1753b33d567b2e57024afbbfa988d7d1801664
Nolen Johnson <johnsonnolen@gmail.com>
2025-06-27 00:36:53 -0400

    motorola: Import configurable touch HAL from hardware/xiaomi
    * Kanged, `sed -i s/xiaomi/motorola/ *`, no other changes.
    * This is a squash of the following commits:
    
    Change-Id: I57865b33cce8046446da81c9ff3fb4039d43e53e
    
    ---
        From b1f26c0899f4f928b5c36c288cde11f459e3ac46 Mon Sep 17 00:00:00 2001
        From: Ramii Ahmed <ramy@ahmedramy.com>
        Date: Tue, 26 Apr 2022 23:43:51 +0000
        Subject: [PATCH] hidl: Implement vendor.lineage.touch@1.0 HAL for Xiaomi
    
        * This can be used by any Xiaomi device that has a touch display that supports multiple sample rates and Xiaomi touch.
        * Most newer Xiaomi phones with high screen refresh rates fit this category:
        * e.g. Mi 10/Pro/Ultra/T/T Pro, Xiaomi SM8350 platform phones & Xiaomi SM6375 phones
    
        Change-Id: Iaf956420157bbc978690fac9053d507ccaea847a
    ---
        From 8a55af6b3ce3b24e1f1ed3f0bf387aa78e8f9eaf Mon Sep 17 00:00:00 2001
        From: luk1337 <priv.luk@gmail.com>
        Date: Tue, 26 Apr 2022 23:51:37 +0000
        Subject: [PATCH] touch: Make HIGH_TOUCH_POLLING_PATH configurable via Soong flag
    
        Change-Id: I7649e084b2604c46f47c07c355913720935e1298
    ---
        From 4d7a95230851c72a777a81b1eea5a7fb640bbfb6 Mon Sep 17 00:00:00 2001
        From: Bruno Martins <bgcngm@gmail.com>
        Date: Sat, 7 Jun 2025 13:40:45 +0100
        Subject: [PATCH] touch: Use select()
    
        Change-Id: I28e54d5ddaf5e376292d2b4ec7b69d5a49fe4f54
    ---
        From: Bruno Martins <bgcngm@gmail.com>
        Date: Sat, 7 Jun 2025 13:55:47 +0100
        Subject: [PATCH] touch: Migrate to AIDL
    
        Change-Id: If44efbab6ad53701a662788daecfd7678935e8b4
    ---
        From 064c24617ef5f2f93583b935c56d835bc0afcabc Mon Sep 17 00:00:00 2001
        From: Bruno Martins <bgcngm@gmail.com>
        Date: Sat, 7 Jun 2025 13:55:47 +0100
        Subject: [PATCH] touch: Migrate to AIDL
    
        Change-Id: If44efbab6ad53701a662788daecfd7678935e8b4
    ---
        From 33a419e8c1809b3965dc9eda3337d05201cc5c4c Mon Sep 17 00:00:00 2001
        From: Bruno Martins <bgcngm@gmail.com>
        Date: Sat, 7 Jun 2025 14:46:40 +0100
        Subject: [PATCH] touch: Include KeyDisabler and KeySwapper generic support
    
        This adds conditional support for KeyDisabler and KeySwapper
        along with HighTouchPollingRate feature.
    
        Change-Id: Ie6c0e7c1032e9d44bf03e6f6738581a4418fb645
    ---
    
    Change-Id: Iea0f415c09e1428d71dea4fefa241abaade295f1

---
+++ aidl/touch/service.cpp
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.touch-service.motorola"
+
+#include "HighTouchPollingRate.h"
+#include "KeyDisabler.h"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using aidl::vendor::lineage::touch::HighTouchPollingRate;
+using aidl::vendor::lineage::touch::KeyDisabler;
+
+int main() {
+    binder_status_t status = STATUS_OK;
+
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+
+#ifdef HTPR_CONTROL_PATH
+    std::shared_ptr<HighTouchPollingRate> htpr = ndk::SharedRefBase::make<HighTouchPollingRate>();
+    const std::string htpr_instance = std::string(HighTouchPollingRate::descriptor) + "/default";
+    status = AServiceManager_addService(htpr->asBinder().get(), htpr_instance.c_str());
+    CHECK_EQ(status, STATUS_OK) << "Failed to add service " << htpr_instance << " " << status;
+#endif
+
+#ifdef KD_CONTROL_PATH
+    std::shared_ptr<KeyDisabler> kd = ndk::SharedRefBase::make<KeyDisabler>();
+    const std::string kd_instance = std::string(KeyDisabler::descriptor) + "/default";
+    status = AServiceManager_addService(kd->asBinder().get(), kd_instance.c_str());
+    CHECK_EQ(status, STATUS_OK) << "Failed to add service " << kd_instance << " " << status;
+#endif
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}
