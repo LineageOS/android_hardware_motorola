919f3efd7934a98afe5130681bb7f314b69b5c8b
Nolen Johnson <johnsonnolen@gmail.com>
2025-06-27 00:48:11 -0400

    motorola: qsap: Run clang-format
    Change-Id: I8c22ae2c1b71a0a68247f75b2052e6dcdf6ac621

--- libqsap_sdk/qsap.c
+++ libqsap_sdk/qsap.c
@@ -6,28 +6,27 @@
 
 #include <string.h>
 
-#include <sys/socket.h>
 #include <linux/wireless.h>
 #include <sys/ioctl.h>
+#include <sys/socket.h>
 
 #include "qsap_api.h"
 
 extern struct Command qsap_str[];
 
-s32 is_softap_enabled(void)
-{
+s32 is_softap_enabled(void) {
     s32 mode = 0;
     int ret;
 
     ret = qsap_get_mode(&mode);
     if (eSUCCESS != ret) {
-       ALOGD("Failed to get the mode of operation\n");
-       return eERR_UNKNOWN;
+        ALOGD("Failed to get the mode of operation\n");
+        return eERR_UNKNOWN;
     }
 
     if (mode == IW_MODE_MASTER) {
-       ALOGD("HOSTAPD Enabled\n");
-       return ENABLE;
+        ALOGD("HOSTAPD Enabled\n");
+        return ENABLE;
     }
 
     ALOGD("HOSTAPD Disabled\n");
@@ -35,17 +34,17 @@ s32 is_softap_enabled(void)
     return DISABLE;
 }
 
-s32 wifi_qsap_set_tx_power(s32 tx_power)
-{
-#define QCSAP_IOCTL_SET_MAX_TX_POWER   (SIOCIWFIRSTPRIV + 22)
+s32 wifi_qsap_set_tx_power(s32 tx_power) {
+#define QCSAP_IOCTL_SET_MAX_TX_POWER (SIOCIWFIRSTPRIV + 22)
     s32 sock;
     s32 ret = eERR_SET_TX_POWER;
-    s8  interface[128];
-    s8  *iface;
+    s8 interface[128];
+    s8* iface;
     s32 len = 128;
     struct iwreq wrq;
 
-    if(NULL == (iface = qsap_get_config_value(CONFIG_FILE, &qsap_str[STR_INTERFACE], interface, (u32*)&len))) {
+    if (NULL == (iface = qsap_get_config_value(CONFIG_FILE, &qsap_str[STR_INTERFACE], interface,
+                                               (u32*)&len))) {
         ALOGE("%s :interface error \n", __func__);
         return ret;
     }
