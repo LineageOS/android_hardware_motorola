5bee2ae7f4e72954e5232ba49d4f9968ab680978
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-08-19 14:39:24 +0000

    sepolicy: Pull sm8475/sm8550 sepolicy rules
    Some duplicate rules needed to be moved to a separate common-um-4.4-5.10
    as qcom/sm8550 and newer sepolicy already defines it
    
    Change-Id: I045b54bae1b52cecc618c06c320ec80040421842

--- sepolicy/qti/SEPolicy.mk
+++ sepolicy/qti/SEPolicy.mk
@@ -3,6 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+include hardware/qcom-caf/common/qcom_defs.mk
+
 SEPOLICY_PLATFORM := $(subst device/qcom/sepolicy_vndr/,,$(SEPOLICY_PATH))
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
@@ -23,4 +25,9 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
     hardware/motorola/sepolicy/qti/public/common-um
+endif
+
+ifneq ($(filter $(UM_4_4_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_FAMILY) $(UM_5_4_FAMILY) $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/motorola/sepolicy/qti/vendor/common-um-4.4-5.10
 endif
