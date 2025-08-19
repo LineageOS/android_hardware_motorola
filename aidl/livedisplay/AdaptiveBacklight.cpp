bca7e4a0478455604e6d15284f0f6830c3a32c60
Giovanni Ricca <giovanniricca@proton.me>
2025-08-19 04:16:53 +0200

    aidl: Initial livedisplay AIDL implementation
    Based on sm7250-common & sm8450-common
    
    Change-Id: Ic044ef4261b2dfac2904609a6eb0ca6635474bc5

---
+++ aidl/livedisplay/AdaptiveBacklight.cpp
@@ -0,0 +1,69 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "AdaptiveBacklightService"
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+#include <livedisplay/motorola/AdaptiveBacklight.h>
+
+using ::android::base::ReadFileToString;
+using ::android::base::Trim;
+using ::android::base::WriteStringToFile;
+
+namespace aidl {
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+
+static constexpr const char* kFileAcl =
+        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dsi_display_acl";
+static constexpr const char* kFileCabc =
+        "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dsi_display_cabc";
+
+AdaptiveBacklight::AdaptiveBacklight() {
+    if (!access(kFileAcl, R_OK | W_OK)) {
+        file_ = kFileAcl;
+    } else if (!access(kFileCabc, R_OK | W_OK)) {
+        file_ = kFileCabc;
+    } else {
+        file_ = nullptr;
+    }
+}
+
+ndk::ScopedAStatus AdaptiveBacklight::getEnabled(bool* _aidl_return) {
+    if (file_ == nullptr) {
+        *_aidl_return = false;
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    std::string tmp;
+    int32_t contents = 0;
+
+    if (ReadFileToString(file_, &tmp)) {
+        contents = std::stoi(Trim(tmp));
+    }
+
+    *_aidl_return = contents > 0;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus AdaptiveBacklight::setEnabled(bool enabled) {
+    if (file_ == nullptr) {
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+
+    if (!WriteStringToFile(std::to_string(enabled), file_, true)) {
+        LOG(ERROR) << "Failed to set AdaptiveBacklight state";
+        return ndk::ScopedAStatus::fromExceptionCode(EX_UNSUPPORTED_OPERATION);
+    }
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+}  // namespace aidl
