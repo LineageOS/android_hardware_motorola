4d489707c23520cd0fa381b34c31360676a729b6
Giovanni Ricca <giovanniricca@proton.me>
2025-08-19 04:16:53 +0200

    aidl: Initial livedisplay AIDL implementation
    Based on sm7250-common & sm8450-common
    
    Change-Id: Ic044ef4261b2dfac2904609a6eb0ca6635474bc5

---
+++ aidl/livedisplay/service.cpp
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define LOG_TAG "vendor.lineage.livedisplay-service-motorola"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <binder/ProcessState.h>
+#include <livedisplay/motorola/AdaptiveBacklight.h>
+#include <livedisplay/motorola/AntiFlicker.h>
+#include <livedisplay/motorola/SunlightEnhancement.h>
+
+using ::aidl::vendor::lineage::livedisplay::AdaptiveBacklight;
+using ::aidl::vendor::lineage::livedisplay::AntiFlicker;
+using ::aidl::vendor::lineage::livedisplay::SunlightEnhancement;
+
+int main() {
+    android::ProcessState::self()->setThreadPoolMaxThreadCount(1);
+    android::ProcessState::self()->startThreadPool();
+
+    LOG(INFO) << "LiveDisplay HAL service is starting.";
+
+    std::shared_ptr<AdaptiveBacklight> ab =
+            ENABLE_AB ? ndk::SharedRefBase::make<AdaptiveBacklight>() : nullptr;
+    std::shared_ptr<AntiFlicker> af = ENABLE_AF ? ndk::SharedRefBase::make<AntiFlicker>() : nullptr;
+    std::shared_ptr<SunlightEnhancement> se =
+            ENABLE_SE ? ndk::SharedRefBase::make<SunlightEnhancement>() : nullptr;
+
+    if (ab) {
+        std::string instance = std::string() + AdaptiveBacklight::descriptor + "/default";
+        binder_status_t status = AServiceManager_addService(ab->asBinder().get(), instance.c_str());
+        CHECK_EQ(status, STATUS_OK);
+    }
+
+    if (af) {
+        std::string instance = std::string() + AntiFlicker::descriptor + "/default";
+        binder_status_t status = AServiceManager_addService(ab->asBinder().get(), instance.c_str());
+        CHECK_EQ(status, STATUS_OK);
+    }
+
+    if (se) {
+        std::string instance = std::string() + SunlightEnhancement::descriptor + "/default";
+        binder_status_t status = AServiceManager_addService(se->asBinder().get(), instance.c_str());
+        CHECK_EQ(status, STATUS_OK);
+    }
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}
