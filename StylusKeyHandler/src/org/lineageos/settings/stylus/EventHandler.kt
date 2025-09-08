aa35ef3eea258e12e9c482a8e654a3acb218d197
AnierinB <anierin@evolution-x.org>
2025-09-08 23:45:53 +0000

    StylusKeyHandler: Make turning screen on a preference
    Change-Id: I8762cdfc3715743c12f213f278895bf0805c1d52
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ StylusKeyHandler/src/org/lineageos/settings/stylus/EventHandler.kt
@@ -0,0 +1,48 @@
+<<<<<<< HEAD   (ba0a7e sepolicy: qti: Label DC dimming LiveDisplay sysfs node)
+=======
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.stylus
+
+import android.content.Context
+
+class EventHandler(private val packageContext: Context) {
+
+    private val utils = Utils(packageContext)
+
+    private val sharedPreferences
+        get() = packageContext.getSharedPreferences(
+            packageContext.packageName + "_preferences",
+            Context.MODE_PRIVATE or Context.MODE_MULTI_PROCESS
+        )
+
+    fun handleRemoved() {
+        if (sharedPreferences.getBoolean(KEY_VIBRATE_WHEN_REMOVED, true)) {
+            utils.vibrateIfNeeded(Utils.VIBRATE_HEAVY_CLICK)
+        }
+        if (sharedPreferences.getBoolean(KEY_SCREEN_ON_WHEN_REMOVED, true)) {
+            utils.turnScreenOn()
+        }
+        val packageName = sharedPreferences.getString(KEY_LAUNCH_APP, null)
+        if (packageName != null) {
+            utils.launchApp(packageName)
+        }
+    }
+
+    fun handleInserted() {
+        if (sharedPreferences.getBoolean(KEY_VIBRATE_WHEN_INSERTED, true)) {
+            utils.vibrateIfNeeded(Utils.VIBRATE_HEAVY_CLICK)
+        }
+    }
+
+    companion object {
+        const val KEY_VIBRATE_WHEN_REMOVED = "vibrate_when_removed"
+        const val KEY_VIBRATE_WHEN_INSERTED = "vibrate_when_inserted"
+        const val KEY_SCREEN_ON_WHEN_REMOVED = "screen_on_when_removed"
+        const val KEY_LAUNCH_APP = "launch_app"
+    }
+}
+>>>>>>> CHANGE (4e6537 StylusKeyHandler: Make turning screen on a preference)
