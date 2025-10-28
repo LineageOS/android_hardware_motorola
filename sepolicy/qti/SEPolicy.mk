LineageOS/android_hardware_oplus

sepolicy: qti: Add platform specific dirs
This will let us put um or legacy-um rules in its own dir.

Change-Id: I9e48638c49f0e1727927e4077c56402ad4dee33f

sepolicy/qti/SEPolicy.mk
@@ -1,8 +1,22 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-BOARD_VENDOR_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/vendor
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/oplus/sepolicy/qti/public
+ifeq ($(SEPOLICY_PATH), device/qcom/sepolicy_vndr-legacy-um)
+    SEPOLICY_PLATFORM := legacy-um
+else
+    SEPOLICY_PLATFORM := um
+endif
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/vendor \
+    hardware/oplus/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/private \
+    hardware/oplus/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
+    hardware/oplus/sepolicy/qti/public \
+    hardware/oplus/sepolicy/qti/public/$(SEPOLICY_PLATFORM)

LineageOS/android_hardware_motorola

sepolicy/qti/SEPolicy.mk
@@ -6,13 +6,16 @@
 SEPOLICY_PLATFORM := $(subst device/qcom/sepolicy_vndr/,,$(SEPOLICY_PATH))
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    hardware/motorola/sepolicy/qti/vendor
+    hardware/motorola/sepolicy/qti/vendor \
+    hardware/motorola/sepolicy/qti/vendor/$(SEPOLICY_PLATFORM)
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    hardware/motorola/sepolicy/qti/private
+    hardware/motorola/sepolicy/qti/private \
+    hardware/motorola/sepolicy/qti/private/$(SEPOLICY_PLATFORM)
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    hardware/motorola/sepolicy/qti/public
+    hardware/motorola/sepolicy/qti/public \
+    hardware/motorola/sepolicy/qti/public/$(SEPOLICY_PLATFORM)
 
 ifneq ($(SEPOLICY_PLATFORM), legacy-um)
 BOARD_VENDOR_SEPOLICY_DIRS += \