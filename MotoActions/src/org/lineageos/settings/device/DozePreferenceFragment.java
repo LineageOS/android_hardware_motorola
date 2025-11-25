98fc84ef15286f9f523f04aaab292e33f2603152
Bruno Martins <bgcngm@gmail.com>
2025-11-25 20:56:05 +0000

    MotoActions: Use new method to listen for preference changes
    Usage of addOnSwitchChangeListener(OnCheckedChangeListener) is
    deprecated. The Javadoc explicitly says:
    
      Use setOnPreferenceChangeListener(OnPreferenceChangeListener) instead.
    
    Change-Id: I71e0e3f86ff339a312cac1e09106f6d5d15b7e43

--- MotoActions/src/org/lineageos/settings/device/DozePreferenceFragment.java
+++ MotoActions/src/org/lineageos/settings/device/DozePreferenceFragment.java
@@ -13,8 +13,6 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
@@ -24,9 +22,7 @@ import androidx.preference.SwitchPreferenceCompat;
 import com.android.settingslib.widget.MainSwitchPreference;
 
 public class DozePreferenceFragment extends PreferenceFragmentCompat
-        implements Preference.OnPreferenceChangeListener, OnCheckedChangeListener {
-
-    private MainSwitchPreference mSwitchBar;
+        implements Preference.OnPreferenceChangeListener {
 
     private SwitchPreferenceCompat mAlwaysOnDisplayPreference;
 
@@ -46,9 +42,9 @@ public class DozePreferenceFragment extends PreferenceFragmentCompat
 
         boolean dozeEnabled = MotoActionsSettings.isDozeEnabled(getActivity());
 
-        mSwitchBar = (MainSwitchPreference) findPreference(MotoActionsSettings.DOZE_ENABLE);
-        mSwitchBar.addOnSwitchChangeListener(this);
-        mSwitchBar.setChecked(dozeEnabled);
+        MainSwitchPreference switchBar = findPreference(MotoActionsSettings.DOZE_ENABLE);
+        switchBar.setOnPreferenceChangeListener(this);
+        switchBar.setChecked(dozeEnabled);
 
         mAlwaysOnDisplayPreference = findPreference(MotoActionsSettings.ALWAYS_ON_DISPLAY);
         mAlwaysOnDisplayPreference.setEnabled(dozeEnabled);
@@ -78,28 +74,24 @@ public class DozePreferenceFragment extends PreferenceFragmentCompat
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        boolean isChecked = (Boolean) newValue;
         if (MotoActionsSettings.ALWAYS_ON_DISPLAY.equals(preference.getKey())) {
             MotoActionsSettings.enableAlwaysOn(getActivity(), (Boolean) newValue);
+        } else if (MotoActionsSettings.DOZE_ENABLE.equals(preference.getKey())) {
+            MotoActionsSettings.enableDoze(getActivity(), isChecked);
+
+            if (!isChecked) {
+                MotoActionsSettings.enableAlwaysOn(getActivity(), false);
+                mAlwaysOnDisplayPreference.setChecked(false);
+            }
+            mAlwaysOnDisplayPreference.setEnabled(isChecked);
+
+            mHandwavePreference.setEnabled(isChecked);
+            mPickUpPreference.setEnabled(isChecked);
+            mPocketPreference.setEnabled(isChecked);
         }
 
         return true;
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        MotoActionsSettings.enableDoze(getActivity(), isChecked);
-
-        mSwitchBar.setChecked(isChecked);
-
-        if (!isChecked) {
-            MotoActionsSettings.enableAlwaysOn(getActivity(), false);
-            mAlwaysOnDisplayPreference.setChecked(false);
-        }
-        mAlwaysOnDisplayPreference.setEnabled(isChecked);
-
-        mHandwavePreference.setEnabled(isChecked);
-        mPickUpPreference.setEnabled(isChecked);
-        mPocketPreference.setEnabled(isChecked);
     }
 
     public static class HelpDialogFragment extends DialogFragment {
