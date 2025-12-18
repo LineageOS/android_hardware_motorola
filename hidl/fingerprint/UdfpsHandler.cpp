b0fc4b02438c66df6e33e3416016dff6d7eb0b11
Nick Reuter <nreuter85@gmail.com>
2025-12-17 23:03:12 -0600

    hidl: biometrics: fingerprint: Support loading handler variants
     * Specify the variant by setting persist.vendor.fingerprint.udfps.handler=<variant>
     * Variant lib must be named "libudfpshandler_<variant>"
     * Attempt to load libudfpshandler.so if no variant is defined
       or fails to load.
    
    Change-Id: I9edef5afaa31608d0ae8032aee2938744ae836db

--- hidl/fingerprint/UdfpsHandler.cpp
+++ hidl/fingerprint/UdfpsHandler.cpp
@@ -5,31 +5,35 @@
  */
 
 #include "UdfpsHandler.h"
+#include <android-base/properties.h>
 #include <dlfcn.h>
 
-#define UDFPS_HANDLER_LIB_NAME "libudfpshandler.so"
+#define UDFPS_HANDLER_DEFAULT "libudfpshandler"
 #define UDFPS_HANDLER_FACTORY "UDFPS_HANDLER_FACTORY"
 
 UdfpsHandlerFactory* getUdfpsHandlerFactory() {
-    void* libudfpshander;
-    UdfpsHandlerFactory* factory_handler;
+    void* libudfpshander = nullptr;
 
-    libudfpshander = dlopen(UDFPS_HANDLER_LIB_NAME, RTLD_LAZY);
-    if (!libudfpshander) {
-        goto error;
+    auto tryLoad = [&](const std::string& name) -> UdfpsHandlerFactory* {
+        std::string lib = name + ".so";
+        libudfpshander = dlopen(lib.c_str(), RTLD_LAZY);
+        if (!libudfpshander) return nullptr;
+
+        auto* factory_handler = reinterpret_cast<UdfpsHandlerFactory*>(
+                dlsym(libudfpshander, UDFPS_HANDLER_FACTORY));
+
+        if (!factory_handler) {
+            dlclose(libudfpshander);
+            libudfpshander = nullptr;
+            return nullptr;
+        }
+        return factory_handler;
+    };
+
+    auto variant = android::base::GetProperty("persist.vendor.fingerprint.udfps.handler", "");
+    if (!variant.empty()) {
+        auto* factory_handler = tryLoad(std::string(UDFPS_HANDLER_DEFAULT) + "_" + variant);
+        if (factory_handler) return factory_handler;
     }
-
-    factory_handler = (UdfpsHandlerFactory*)dlsym(libudfpshander, UDFPS_HANDLER_FACTORY);
-    if (!factory_handler) {
-        goto error;
-    }
-
-    return factory_handler;
-
-error:
-    if (libudfpshander) {
-        dlclose(libudfpshander);
-    }
-
-    return nullptr;
+    return tryLoad(UDFPS_HANDLER_DEFAULT);
 }
