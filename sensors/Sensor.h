3b57607869b0e9d16f38adfee99be982233a1dd3
Nick Reuter <nreuter85@gmail.com>
2025-12-26 16:23:49 -0600

    sensors: Use soong select to support various configurations
    Change-Id: I652471dbbbc89d0f0175aa46f310d7fc73d452ee

--- sensors/Sensor.h
+++ sensors/Sensor.h
@@ -129,6 +129,7 @@ const std::string kTsDoubleTapEnabledPath = kTsPath + "double_tap_enabled";
 
 constexpr int32_t SENSOR_TYPE_BASE = static_cast<int32_t>(SensorType::DEVICE_PRIVATE_BASE) + 100;
 
+#ifdef ENABLE_DOUBLE_TAP
 class DoubleTapSensor : public SysfsPollingOneShotSensor {
   public:
     DoubleTapSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
@@ -137,10 +138,12 @@ class DoubleTapSensor : public SysfsPollingOneShotSensor {
               "Double Tap Sensor", "org.lineageos.sensor.double_tap",
               static_cast<SensorType>(SENSOR_TYPE_BASE + 1)) {}
 };
+#endif
 
 const std::string kTsUdfpsPressedPath = kTsPath + "udfps_pressed";
 const std::string kTsUdfpsEnabledPath = kTsPath + "udfps_enabled";
 
+#ifdef ENABLE_UDFPS
 class UdfpsSensor : public SysfsPollingOneShotSensor {
   public:
     UdfpsSensor(int32_t sensorHandle, ISensorsEventCallback* callback)
@@ -149,6 +152,7 @@ class UdfpsSensor : public SysfsPollingOneShotSensor {
               "UDFPS Sensor", "org.lineageos.sensor.udfps",
               static_cast<SensorType>(SENSOR_TYPE_BASE + 2)) {}
 };
+#endif
 
 }  // namespace implementation
 }  // namespace subhal
