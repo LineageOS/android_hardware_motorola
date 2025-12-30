6b7da6b6901aa220da18c9af7af9b21f5e252126
Nick Reuter <nreuter85@gmail.com>
2025-12-30 17:18:40 -0600

    MotoActions: Update the BroadcastReceiver to a variable
    Change-Id: Ib62fa2f9d96eb138e34712343a76a917bc87a411

--- MotoActions/src/org/lineageos/settings/device/actions/FlipToMute.kt
+++ MotoActions/src/org/lineageos/settings/device/actions/FlipToMute.kt
@@ -34,8 +34,6 @@ class FlipToMute(
     private var isStowed = false
     private var filter = notificationManager.currentInterruptionFilter
 
-    private val receiver = Receiver()
-
     override fun updateState() {
         when {
             motoActionsSettings.isFlipToMuteEnabled() && !isEnabled -> {
@@ -97,7 +95,7 @@ class FlipToMute(
         }
     }
 
-    inner class Receiver : BroadcastReceiver() {
+    private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             if (!isFlatDown && !isStowed) {
                 filter = notificationManager.currentInterruptionFilter
