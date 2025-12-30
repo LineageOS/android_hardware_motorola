4dd2545798a7689d2fdfc3a72a6f4e915ad371f6
Nick Reuter <nreuter85@gmail.com>
2025-12-30 17:41:03 -0600

    MotoActions: Switch from PhoneStateListener to TelephonyCallbacks
     PhoneStateListener was deprecated in api 31
    
    Change-Id: Ideb6137ce3f0bbf926e5e7d0ffdf1ed45ac7111a

--- MotoActions/src/org/lineageos/settings/device/actions/LiftToSilence.kt
+++ MotoActions/src/org/lineageos/settings/device/actions/LiftToSilence.kt
@@ -11,7 +11,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.telecom.TelecomManager
-import android.telephony.PhoneStateListener
+import android.telephony.TelephonyCallback
 import android.telephony.TelephonyManager
 import android.util.Log
 import org.lineageos.settings.device.MotoActionsSettings
@@ -19,9 +19,9 @@ import org.lineageos.settings.device.SensorHelper
 
 class LiftToSilence(
     private val motoActionsSettings: MotoActionsSettings,
-    context: Context,
+    private val context: Context,
     private val sensorHelper: SensorHelper,
-) : PhoneStateListener(), UpdatedStateNotifier {
+) : UpdatedStateNotifier {
 
     private val telecomManager = context.getSystemService(TelecomManager::class.java)
     private val telephonyManager = context.getSystemService(TelephonyManager::class.java)
@@ -34,29 +34,32 @@ class LiftToSilence(
 
     override fun updateState() {
         if (motoActionsSettings.isLiftToSilenceEnabled()) {
-            telephonyManager.listen(this, LISTEN_CALL_STATE)
+            telephonyManager.registerTelephonyCallback(context.mainExecutor, callStateListener)
         } else {
-            telephonyManager.listen(this, 0)
+            telephonyManager.unregisterTelephonyCallback(callStateListener)
         }
     }
 
-    @Synchronized
-    override fun onCallStateChanged(state: Int, incomingNumber: String?) {
-        when {
-            state == TelephonyManager.CALL_STATE_RINGING && !isRinging -> {
-                Log.d(TAG, "Ringing started")
-                sensorHelper.registerListener(flatUpSensor, flatUpListener)
-                sensorHelper.registerListener(stowSensor, stowListener)
-                isRinging = true
-            }
-            state != TelephonyManager.CALL_STATE_RINGING && isRinging -> {
-                Log.d(TAG, "Ringing stopped")
-                sensorHelper.unregisterListener(flatUpListener)
-                sensorHelper.unregisterListener(stowListener)
-                isRinging = false
+    private val callStateListener =
+        object : TelephonyCallback(), TelephonyCallback.CallStateListener {
+            @Synchronized
+            override fun onCallStateChanged(state: Int) {
+                when {
+                    state == TelephonyManager.CALL_STATE_RINGING && !isRinging -> {
+                        Log.d(TAG, "Ringing started")
+                        sensorHelper.registerListener(flatUpSensor, flatUpListener)
+                        sensorHelper.registerListener(stowSensor, stowListener)
+                        isRinging = true
+                    }
+                    state != TelephonyManager.CALL_STATE_RINGING && isRinging -> {
+                        Log.d(TAG, "Ringing stopped")
+                        sensorHelper.unregisterListener(flatUpListener)
+                        sensorHelper.unregisterListener(stowListener)
+                        isRinging = false
+                    }
+                }
             }
         }
-    }
 
     private val flatUpListener =
         object : SensorEventListener {
