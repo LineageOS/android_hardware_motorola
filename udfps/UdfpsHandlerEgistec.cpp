bed6a217c037d59ec54b3e86cbf6c7c13b28b125
Marc Bourgoin <themard85@gmail.com>
2026-02-05 22:04:17 -0700

    udfps: Allow devices to sleep before enabling HBM on FOD
    A16 triggers screen off UDFPS immediately instead of waiting for the screen to wake up
    which causes some optical UDFPSs to fail to trigger HBM due to the screen not waking
    up fast enough, so lets add a sleep to give the screen a chance to wake up first.
    
    To enable set the following:
    $(call soong_config_set,moto_udfps,screen_wake_delay,200) // Example setting a 200ms sleep
    
    Issue potentially caused by this commit:
    https://android.googlesource.com/platform/frameworks/base/+/2e82f90d77eb0b028822d37dc0c7d37a1bb1f6a1
    
    Change-Id: Ib87a9443716d3bcca4ee6aa34f73b03801046f09

--- udfps/UdfpsHandlerEgistec.cpp
+++ udfps/UdfpsHandlerEgistec.cpp
@@ -25,6 +25,9 @@ class EgistecUdfpsHandler : public UdfpsHandler {
     }
 
     void onFingerDown(uint32_t /*x*/, uint32_t /*y*/, float /*minor*/, float /*major*/) {
+        if (SCREEN_WAKE_DELAY > 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(SCREEN_WAKE_DELAY));
+        }
         enableHighBrightFod();
         std::thread([this]() {
             std::this_thread::sleep_for(std::chrono::milliseconds(200));
